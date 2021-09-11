7d574044a9ca79cbee01d8b113b3557d61a45eed
Renan Rosas <renan-rosas@hotmail.com>
2021-09-11 20:27:08 -0300

    backend initial created

---
+++ backend/src/main/java/com/renanrosas/blogspringangular/BlogSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.renanrosas.blogspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpringAngularApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/renanrosas/blogspringangular/BlogSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanrosas.blogspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
